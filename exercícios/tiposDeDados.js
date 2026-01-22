877fba062710dbc93bcd9f45159272bbc769af2d
gustavofrancabraga <gustavofrancabraga04@gmail.com>
2026-01-22 16:00:22 -0300

    adicionando novo arquivo tiposDeDados.js e removendo variaveis.js

---
+++ exercícios/tiposDeDados.js
@@ -0,0 +1,12 @@
+const nomeFilme = "Superman";
+
+const anoLançamento = 2025;
+
+let nota = 10;
+
+const assistiu = true;
+
+console.log("Título do filme: " + nomeFilme);
+console.log("Ano de lançamento: " + anoLançamento);
+console.log("Nota: " + nota);
+console.log("Assistiu: " + assistiu);
--- variaveis/variaveis.js
+++
@@ -1,5 +0,0 @@
-var feiticeiroMaisForte = "Satoru Gojo";
-var estudantMaisForte = "Yuji Itadori";
-
-console.log(feiticeiroMaisForte);
-console.log(estudantMaisForte);
