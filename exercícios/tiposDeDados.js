8137459e128609483cf40ab70a22f5a72a53f091
gustavofrancabraga <gustavofrancabraga04@gmail.com>
2026-01-22 16:06:39 -0300

    praticando exercícios

--- exercícios/tiposDeDados.js
+++ exercícios/tiposDeDados.js
@@ -1,3 +1,5 @@
+// Exercício 01
+
 const nomeFilme = "Superman";
 
 const anoLançamento = 2025;
@@ -10,3 +12,34 @@ console.log("Título do filme: " + nomeFilme);
 console.log("Ano de lançamento: " + anoLançamento);
 console.log("Nota: " + nota);
 console.log("Assistiu: " + assistiu);
+
+// Exercício 02
+
+const nomeSérie = "Pacificador";
+
+const temporadas = 2;
+
+let avaliação = 9.5;
+
+const assistiuSérie = false;
+
+console.log("Título da série: " + nomeSérie);
+console.log("Número de temporadas: " + temporadas);
+console.log("Avaliação: " + avaliação);
+console.log("Assistiu a série: " + assistiuSérie);
+
+// Exercício 03
+
+const anime = "Jujutsu Kaisen";
+
+const episódios = 24;
+
+let classificação = 9.8;
+
+const assistiuAnime = true;
+
+console.log("Título do anime: " + anime);
+console.log("Número de episódios: " + episódios);
+console.log("Classificação: " + classificação);
+console.log("Assistiu o anime: " + assistiuAnime);      
+
